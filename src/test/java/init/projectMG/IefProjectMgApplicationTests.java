39fa70e0c0e7ceb10aa4ff88316aba0523f03753
Iuri.Ferrari <iuriestevam@hotmail.com>
2024-09-05 01:30:07 -0300

    050924 - remake: project started from scracth

---
+++ src/test/java/init/projectMG/IefProjectMgApplicationTests.java
@@ -0,0 +1,13 @@
+package init.projectMG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IefProjectMgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
