a9d415d88ad749fd2aae8608292be76f6c39acf3
Iuri Ferrari <iurieferrari@gmail.com>
2025-11-07 19:24:47 -0300

    Fixing Bugs

--- src/main/java/com/projectmg/Services/MaterialService.java
+++ src/main/java/com/projectmg/Services/MaterialService.java
@@ -70,6 +70,7 @@ public class MaterialService {
                 ultimoComentario,
                 m.getLimiteMinimo(),
                 m.isAtivo()
+                m.getDataProgramadaCompra()
         );
     }
 
